7982e588b2a8b1f69aa6c60ab9368db492c6d7b1
Martin Kirov <fas7blas7@gmail.com>
2025-05-23 07:32:18 +0300

    Adding TopIntegers task

---
+++ 07.TopIntegers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _07.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToArray();
+
+            // 5 2 8
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i]; // 5
+                bool isTopNumber = true;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int nextNumber = numbers[j]; // 2
+                    if (currentNumber <= nextNumber)
+                    {
+                        isTopNumber = false;
+                        break;
+                    }
+                }
+
+                if (isTopNumber)
+                {
+                    Console.Write($"{currentNumber} ");
+                }
+            }
+        }
+    }
+}
